5e227c8897fb3d2c1275eddce88b386b7b9e9ae0
Hemant Choudhary <globalgrowth07@gmail.com>
2022-01-13 10:02:48 +0000

    completed the easy version

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = ""
+
+for char in range(1, nr_letters + 1):
+  password += random.choice(letters)
+for char in range(1, nr_symbols + 1):
+  password += random.choice(symbols)
+for char in range(1, nr_numbers + 1):
+  password += random.choice(numbers)
+print(password)
+  
